71923fe847e452a6f1c11c5d4a9af33e31b0431d
co-lo-e <ko000029@algonquinlive.com>
2025-07-13 14:50:06 -0400

    env example with hr

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import cors from "cors";
 import express from "express";
+import path from "node:path";
 import { testHR } from "./orcl";
 
 
 const app = express();
 app.use(cors())
 
-app.get("/", (req, resp) => {
-	resp.send("Hello world")
-})
+app.use(express.static(path.join(__dirname, "public")))
+
+// app.get("/", (req, resp) => {
+// 	resp.send("Hello world")
+// })
 
 const port = 8000;
 app.listen(port, async () => {
